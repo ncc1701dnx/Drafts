cef9c5b90ac09892591a80553e5d154bafd1d348
Chenrui Xie <31363084+ncc1701dnx@users.noreply.github.com>
2024-04-09 15:49:00 +0200

    work_0901_draw_patients_number_HMP2.R

--- draw_figure/work_0901_draw_patients_number_HMP2.R
+++ draw_figure/work_0901_draw_patients_number_HMP2.R
@@ -1,8 +1,8 @@
-#### HMP2 病人分布（和paper差别）
-# 加载ggplot2包
+#### HMP2 patient distribution（find difference with paper）
+# Load ggplot2
 library(ggplot2)
 
-# 初始化变量和点的数量
+# initialiying number of dots and samples
 n_rows = 8
 n_cols = ceiling(117 / n_rows)
 
@@ -11,14 +11,14 @@ n_abundance_based = 5
 n_metadata_based = 14
 n_other = 117 - n_shared - n_abundance_based - n_metadata_based
 
-# 创建数据框
+# generate the data frame
 data <- data.frame(
   x = integer(0),
   y = integer(0),
   group = character(0)
 )
 
-# 填充数据框
+# fill the data frame
 counter = 1
 for (col in 1:n_cols) {
   for (row in 1:n_rows) {
@@ -45,9 +45,9 @@ for (col in 1:n_cols) {
 }
 
 my_colors <- brewer.pal(11, "Spectral")
-my_colors <- my_colors[c( 9, 10, 11, 7)] # 手动定义颜色
+my_colors <- my_colors[c( 9, 10, 11, 7)] # we can manuallz define the clolor settings
 
-# 使用ggplot2进行绘图
+# Draw plots with ggplot2
 ggplot(data, aes(x = x, y = y, color = group)) +
   geom_point(size = 20, shape = 15) +
   theme_minimal() +
@@ -64,23 +64,23 @@ ggplot(data, aes(x = x, y = y, color = group)) +
 ### cohort1
 
 29+15
-# 初始化变量和点的数量
+# 
 n_rows = 5
 n_cols = ceiling(44 / n_rows)
 
 n_AZA_6MP_user_remission = 3
-n_AZA_6MP_user = 15 - n_AZA_6MP_user_remission  # 减去进入缓解的患者
+n_AZA_6MP_user = 15 - n_AZA_6MP_user_remission  # exclude those patients who are in remission
 #n_UC_patient = 40 - n_AZA_6MP_user - n_AZA_6MP_user_remission
 n_UC_patient = 29
 
-# 创建数据框
+# 
 data <- data.frame(
   x = integer(0),
   y = integer(0),
   group = character(0)
 )
 
-# 填充数据框
+# fill our data frame
 counter = 1
 for (row in 1:n_rows) {
   for (col in 1:n_cols) {
@@ -117,7 +117,7 @@ ggplot(data, aes(x = x, y = y, color = group)) +
         legend.title = element_blank()) +
   labs(title = "Cohort2 Patient Distribution") +
   #scale_color_manual(values = my_colors) +
-  coord_fixed(ratio = 1.5) + # 调整坐标轴的比例
+  coord_fixed(ratio = 1.5) + 
   theme(text=element_text(size=20, face = "bold"),
         legend.position="right",
         strip.text = element_text(face = "bold"))
